6249dd171edeaa5a22ef43e2b22e9c07c87e2fd5
aww <vovapodkorytov966@gmail.com>
2024-06-09 04:33:21 +0300

    [TASK-3] Idk

--- LBFGS.py
+++ LBFGS.py
@@ -1,9 +1,12 @@
 import random
 import sys
 import time
+from functools import reduce
+import operator
 
 import numpy as np
 from scipy.optimize import approx_fprime
+from Gradient_descent import choose_step
 
 
 class ParametrizedFunction:
@@ -38,97 +41,135 @@ def get_vertical(arr: np.ndarray) -> np.ndarray:
     return np.asmatrix(arr).transpose()
 
 
-def take_step(s: list[np.ndarray],
+def get_index(k: int, m: int, i: int) -> int:
+    """
+    [k - m + 1, k] -> [0, m - 1]
+
+    :param k:
+    :param m:
+    :param i:
+    :return:
+    """
+    if k < m:
+        return i - (k - m + 1)
+    return i - 1
+
+
+def get_V_i(N: int,
+            i: int,
+            s: list[np.ndarray],
+            y: list[np.ndarray],
+            rhos: list[float]) -> np.matrix:
+    I = np.identity(N)
+    if len(s) > 0:
+        return I - rhos[i - 1] * get_vertical(y[i - 1]) * s[i - 1]
+    return np.asmatrix(I)
+
+
+def take_step(x: np.ndarray,
+              function: ParametrizedFunction,
+              H: np.matrix,
+              s: list[np.ndarray],
               y: list[np.ndarray],
               rhos: list[float],
-              function: ParametrizedFunction,
-              start: list[float],
-              m: int) -> np.ndarray:
-    q = function.gradient(start)
-    alphas = []
-    for i in range(m - 1, -1, -1):
-        alpha = (rhos[i] * s[i] * get_vertical(q))[0, 0]
-        print(f"alpha = {alpha}")
-        alphas.append(alpha)
-        q -= alpha * y[i]
+              k: int,
+              m: int) -> tuple[np.ndarray, np.matrix]:
+    grad = function.gradient(list(x))
+    d = np.asarray(-H * get_vertical(grad)).transpose()[0]
+    print(f"grad = {grad}")
+    print(f"H = {H}")
+    print(f"x = {x}")
+    print(f"d = {d}")
+    step = choose_step(list(x), grad, function.compute)
+    x_next = x + step * d
 
-    print(f"q = {q}")
-    print(f"s = {s}")
-    print(f"y = {y}")
-    print(f"alpha = {alphas}")
-    print(f"rhos = {rhos}")
+    m1 = min(k, m - 1)
+    N = function.get_N()
+    # V = get_V_i(N, 0, s, y, rhos).transpose()
+    # for i in range(1, m1 + 1):
+    #     print(k, m1, i, k - i, k - m1)
+    #     V_i = get_V_i(N, k - i, s, y, rhos).transpose()
+    #     V *= V_i
+    # V_right = get_V_i(N, 1, s, y, rhos)
+    # for i in range(1, k + 1):
+    #     V_i = get_V_i(N, i, s, y, rhos)
+    #     V_right *= V_i
+    vs = [get_V_i(N, j, s, y, rhos).transpose() for j in range(m1, -1, -1)]
+    left = reduce(operator.mul, vs, np.identity(N))
+    vs_right = [get_V_i(N, j, s, y, rhos) for j in range(1, m1 + 1)]
+    right = reduce(operator.mul, vs_right, np.identity(N))
+    H_new = left * H * right
 
-    gamma = ((s[-1] * get_vertical(y[-1])) / (y[-1] * get_vertical(y[-1])))[0, 0]
-    H = gamma * np.ones((function.get_N(), function.get_N()))
-    z = H * get_vertical(q)
+    # for i in range(k - m1, k + 1):
+    for i in range(0, m1 + 1):
+        if len(s) > 0:
+            center = get_vertical(s[i - 1]) * s[i - 1]
+            vs = [get_V_i(N, j, s, y, rhos).transpose() for j in range(m1, -1, -1)]
+            left = reduce(operator.mul, vs, np.ones((N, N)))
+            vs_right = [get_V_i(N, j, s, y, rhos) for j in range(1, m1 + 1)]
+            right = reduce(operator.mul, vs_right, np.ones((N, N)))
+            H_new += rhos[i - 1] * left * center * right
+        else:
+            H_new = H
+    return x_next, H_new
 
-    print(f"z = {z}")
 
-    for i in range(m):
-        beta = (rhos[-1] * y[i] * z)[0, 0]
-        z += get_vertical(s[i]) * (alphas[i] - beta)
+def L_BFGS(start: list[float], function: ParametrizedFunction, m: int, eps: float = 1e-6) -> np.ndarray:
+    N = function.get_N()
+    H = np.identity(N)
+    s = []
+    y = []
+    rhos = []
+    xn = np.asarray(start)
 
-    print(f"result - {np.asarray(z.transpose())[0]}")
-    return np.asarray(z.transpose())[0]
-
-
-def L_BGFGS(start: list[float], function: ParametrizedFunction, m: int, eps: float = 1e-6) -> np.ndarray:
-    values = get_initial_values(start, function)
-    s = [values[0]]
-    y = [values[1]]
-    rhos = [1 / np.dot(s[0], y[0])]
-
-    # print(np.asmatrix(y[0]) * get_vertical(s[0])) # Returns number
-    # print(get_vertical(s[0]) * np.asmatrix(y[0])) # Returns matrix
-
-    # Filling history
-    for i in range(1, m):
-        s.append(s[-1])
-        y.append(y[-1])
-        rhos.append(rhos[-1])
-
-    print("==== Initial values ====")
-    print(f"s = {s}")
-    print(f"y = {y}")
-    print(f"rhos = {rhos}")
-    print()
-    print()
-
-    xn = start.copy()
+    k = 0
     while True:
-        direction = take_step(s, y, rhos, function, xn, m)
-        print(direction)
+        x_next, H_next = take_step(xn, function, H, s, y, rhos, k, m)
+        H = H_next
+
+        print(x_next)
+        print(H_next)
+
+        if len(s) == m:
+            s.pop(0)
+        new_s = x_next - xn
+        s.append(new_s)
+
+        if len(y) == m:
+            y.pop(0)
+        new_y = function.gradient(list(x_next)) - function.gradient(list(xn))
+        y.append(new_y)
+
+        if len(rhos) == m:
+            rhos.pop(0)
+            rho = 1 / (y[-1] * get_vertical(s[-1]))[0, 0]
+            # rhos.append(rho)
+            rhos.append(1)
+        else:
+            rhos.append(1)
+
+        print("Result on this step")
+        print(x_next)
         print(xn)
-        print()
-        x_next = np.asarray(xn) - direction
+        print(function.compute(list(x_next)))
+        print(function.compute(list(xn)))
+        if k > m and abs(function.compute(list(x_next)) - function.compute(list(xn))) < eps:
+            print(f"Finished in {k - m} iterations")
+            return x_next if function.compute(list(x_next)) < function.compute(list(xn)) else xn
 
-        if abs(function.compute(list(x_next)) - function.compute(list(xn))) < eps:
-            return x_next
-
-        s.pop(0)
-        s.append(x_next - xn)
-
-        grad = function.gradient(xn)
-        grad_next = function.gradient(x_next)
-        y.pop(0)
-        y.append(grad_next - grad)
-
-        rho_next = 1 / (y[-1] * get_vertical(s[-1]))
-        rhos.pop(0)
-        rhos.append(rho_next)
-
-        xn = x_next
+        k += 1
 
 
 def test(m: int = 10):
     # Ns = [2, 4, 6]
     Ns = [2]
     for N in Ns:
-        starting_point = np.random.rand(N) * random.randint(-3, 3)
+        starting_point = np.random.rand(N) * random.randint(-5, 5)
         function = ParametrizedFunction(N)
-        argmin = L_BGFGS(list(starting_point), function, m)
+        argmin = L_BFGS(list(starting_point), function, m)
         print("==== Arguments ====")
         print(f"N = {N}, m = {m}")
+        print(f"Starting point = {starting_point}")
         print(f"Minimal value is {function.compute(list(argmin))} at {argmin}")
         print("===================")
 
