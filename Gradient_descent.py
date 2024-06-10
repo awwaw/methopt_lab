6adfafb9d3e27f90cf93e5eecd50d61265e29ef1
aww <vovapodkorytov966@gmail.com>
2024-06-10 19:46:55 +0300

    [TASK-2] Wolfe conds replaced with Adam adjusted gradient

--- Gradient_descent.py
+++ Gradient_descent.py
@@ -6,6 +6,37 @@ import numpy as np
 from scipy.optimize import approx_fprime
 
 
+class AdamParams:
+    def __init__(self, learning_rate: float = 1e-4,
+                 beta1: float = 0.9,
+                 beta2: float = 0.999,
+                 epsilon: float = 1e-8):
+        self.learning_rate = learning_rate
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.m = None
+        self.v = None
+        self.time = 0
+
+
+def update_adam(params: AdamParams, x: np.ndarray, grad: np.ndarray) -> np.ndarray:
+    if params.m is None:
+        params.m = np.zeros_like(grad)
+    if params.v is None:
+        params.v = np.zeros_like(grad)
+
+    params.time += 1
+    params.m = params.beta1 * params.m + (1 - params.beta1) * grad
+    params.v = params.beta2 * params.v + (1 - params.beta2) * grad ** 2
+
+    m = params.m / (1 - params.beta1 ** params.time)
+    v = params.v / (1 - params.beta2 ** params.time)
+
+    adjusted_grad = m / (np.sqrt(v) + params.epsilon)
+    return x - params.learning_rate * adjusted_grad
+
+
 def main():
     rosenbrok_lagrange = Function("(1-x)**2 + 100 * (y-x**2)**2 - z * (y + x**2)")
     constraint = Function("y + x**2")
@@ -16,13 +47,13 @@ def main():
     starting_points = [
         [0.103, -0.01, 0],
         [0.05, -0.05, 0],
-        [-0.01, 0.01, 0],
-        [1, -1, 1.2],
-        [0.23, 0.32, 0]
+        [-0.01, 0.01, 0]
+        # [1, -1, 1.2],
+        # [0.23, 0.32, 0]
     ]
 
     for start in starting_points:
-        result = gradient_descend(rosenbrok_lagrange, constraint, start, 1e-4)
+        result = gradient_descend(rosenbrok_lagrange, constraint, start, 1e-3)
         ans = result[0]
         value = rosenbrok_lagrange.compute(ans)
         print()
@@ -33,48 +64,6 @@ def main():
 
 def gradient(func: Callable[[list[float]], float], point: list[float]):
     return approx_fprime(point, func, 1e-6)
-
-
-def check_wolfe_conditions(alpha: float, x: list[float], f: Callable[[list[float]], float],
-                           direction: np.ndarray[np.float64]):
-    c1 = 0.2
-    c2 = 0.8
-    next = list(np.asarray(x, dtype='float64') + alpha * direction)
-    f_next = f(next)
-    fx = f(x)
-    b = np.dot(gradient(f, x), direction)
-    armijo_rule = f_next <= fx + alpha * c1 * b
-
-    next_grad = gradient(f, next)
-    lhs = np.dot(next_grad, direction)
-    rhs = c2 * b
-    curvature = lhs - rhs >= 1e-5
-    return armijo_rule and curvature
-
-
-def choose_step(current_x: list[float], direction: np.ndarray, f: Callable[[list[float]], float]) -> float:
-    """
-    Choose step for gradient descend using Wolfe conditions
-    https://indrag49.github.io/Numerical-Optimization/line-search-descent-methods.html#the-wolfe-conditions
-    Here I am using some dumb approach with linear search of step sice satisfying Wolfe conditions
-
-    :param current_x:
-    :param direction:
-    :param f:
-    :return:
-    """
-
-    step = 1.
-    coefficient = 0.8
-    iterations = 0
-
-    while True:  # iterations < 50:
-        iterations += 1
-
-        if check_wolfe_conditions(step, current_x, f, direction):
-            return step
-
-        step *= coefficient
 
 
 def normalize(vector: np.ndarray) -> np.ndarray:
@@ -89,24 +78,22 @@ def gradient_descend(f: Function, constraint: Function, start: list[float], eps:
     def norm_grad(point: list[float]):
         return sqrt(sum(map(lambda x: x ** 2, f.gradient(point))))
 
+    params = AdamParams()
     while True:
         iterations += 1
 
         grad = gradient(norm_grad, xn)
-        step = choose_step(xn, grad, norm_grad)
-        # step = get_steplenth(norm_grad, np.asarray(xn), np.asarray(grad))
-        next_x = list(np.asarray(xn) - step * grad)
+        next_x = list(update_adam(params, np.asarray(xn), np.asarray(grad)))
 
-        if iterations == 30 or abs(norm_grad(next_x)) < eps:  # and abs(constraint.compute(next_x[:-1])) < eps:
+        if abs(norm_grad(next_x)) < eps:  # and abs(constraint.compute(next_x[:-1])) < eps:
             return next_x, iterations
 
-        last_x = xn.copy()
         xn = next_x.copy()
 
-        # if iterations % 20 == 0:
-        #     print(f"Iteration {iterations}. Current point is - {xn}, step = {step}")
+        # if iterations % 10_000 == 0:
+        #     print(f"Iteration {iterations}. Current point is - {xn}")
         #     print(f.gradient(xn))
-        #     print(f"Current gradient (direction of descent) - {grad}, it's norm diff = {abs(norm_grad(xn) - norm_grad(last_x))}")
+        #     print(f"Current gradient (direction of descent) - {grad}, it's norm diff = {abs(norm_grad(xn))}")
         #     print(f"Constraint: {constraint.compute(next_x[:-1])}")
         #     print()
 
